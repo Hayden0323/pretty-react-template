75038368949dca09809eeb2b66652a5048c890d0
Hayden Fan <haosong0323@gmail.com>
2021-08-11 00:30:46 +0800

    feat: add ThemeProvider

---
+++ packages/theme/index.tsx
@@ -0,0 +1,117 @@
+import React, { useMemo } from 'react'
+import { css, DefaultTheme, ThemeProvider as StyledComponentsThemeProvider } from 'styled-components'
+import { Colors } from './styled'
+
+export const MEDIA_WIDTHS = {
+  upToExtraSmall: 500,
+  upToSmall: 720,
+  upToMedium: 960,
+  upToLarge: 1280,
+}
+
+const mediaWidthTemplates: { [width in keyof typeof MEDIA_WIDTHS]: typeof css } = Object.keys(MEDIA_WIDTHS).reduce(
+  (accumulator, size) => {
+    ;(accumulator as any)[size] = (a: any, b: any, c: any) => css`
+      @media (max-width: ${(MEDIA_WIDTHS as any)[size]}px) {
+        ${css(a, b, c)}
+      }
+    `
+    return accumulator
+  },
+  {}
+) as any
+
+const white = '#FFFFFF'
+const black = '#000000'
+
+export function colors(darkMode: boolean): Colors {
+  return {
+    // base
+    white,
+    black,
+
+    // text
+    text1: darkMode ? '#FFFFFF' : '#000000',
+    text2: darkMode ? '#C3C5CB' : '#565A69',
+    text3: darkMode ? '#6C7284' : '#888D9B',
+    text4: darkMode ? '#565A69' : '#C3C5CB',
+    text5: darkMode ? '#2C2F36' : '#EDEEF2',
+
+    // backgrounds / greys
+    bg0: darkMode ? '#191B1F' : '#FFF',
+    bg1: darkMode ? '#212429' : '#F7F8FA',
+    bg2: darkMode ? '#2C2F36' : '#EDEEF2',
+    bg3: darkMode ? '#40444F' : '#CED0D9',
+    bg4: darkMode ? '#565A69' : '#888D9B',
+    bg5: darkMode ? '#6C7284' : '#888D9B',
+    bg6: darkMode ? '#1A2028' : '#6C7284',
+
+    //specialty colors
+    modalBG: darkMode ? 'rgba(0,0,0,.425)' : 'rgba(0,0,0,0.3)',
+    advancedBG: darkMode ? 'rgba(0,0,0,0.1)' : 'rgba(255,255,255,0.6)',
+
+    //primary colors
+    primary1: darkMode ? '#2172E5' : '#ff007a',
+    primary2: darkMode ? '#3680E7' : '#FF8CC3',
+    primary3: darkMode ? '#4D8FEA' : '#FF99C9',
+    primary4: darkMode ? '#376bad70' : '#F6DDE8',
+    primary5: darkMode ? '#153d6f70' : '#FDEAF1',
+
+    // color text
+    primaryText1: darkMode ? '#6da8ff' : '#ff007a',
+
+    // secondary colors
+    secondary1: darkMode ? '#2172E5' : '#ff007a',
+    secondary2: darkMode ? '#17000b26' : '#F6DDE8',
+    secondary3: darkMode ? '#17000b26' : '#FDEAF1',
+
+    // other
+    red1: '#FD4040',
+    red2: '#F82D3A',
+    red3: '#D60000',
+    green1: '#27AE60',
+    yellow1: '#e3a507',
+    yellow2: '#ff8f00',
+    yellow3: '#F3B71E',
+    blue1: '#2172E5',
+    blue2: '#5199FF',
+
+    error: '#FD4040',
+    success: '#27AE60',
+    warning: '#ff8f00',
+  }
+}
+
+export function theme(darkMode: boolean): DefaultTheme {
+  return {
+    ...colors(darkMode),
+
+    grids: {
+      sm: 8,
+      md: 12,
+      lg: 24,
+    },
+
+    //shadows
+    shadow1: darkMode ? '#000' : '#2F80ED',
+
+    // media queries
+    mediaWidth: mediaWidthTemplates,
+
+    // css snippets
+    flexColumnNoWrap: css`
+      display: flex;
+      flex-flow: column nowrap;
+    `,
+    flexRowNoWrap: css`
+      display: flex;
+      flex-flow: row nowrap;
+    `,
+  }
+}
+
+export default function ThemeProvider({ children }: { children: React.ReactNode }) {
+  const themeObject = useMemo(() => theme(false), [])
+
+  return <StyledComponentsThemeProvider theme={themeObject}>{children}</StyledComponentsThemeProvider>
+}
